2824de8e2e16d29364c08f33106421d0abd0e14f
Amish Palkar <amishpalkar302001@gmail.com>
2023-12-08 10:17:35 +0530

    Second commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,18 +3,33 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `multicolor`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://thepmsquare.github.io/multicolor`,
+    // TODO
+    description: ``,
   },
+  pathPrefix: "/multicolor",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  graphqlTypegen: false,
+  plugins: [
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `multicolor`,
+        short_name: `multicolor`,
+        start_url: "/",
+        icon: "src/images/multicolor.svg",
+        display: "fullscreen",
+        background_color: `#000000`,
+        theme_color: `#00ffff`,
+        icon_options: {
+          purpose: `any maskable`,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,94 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import React, { FC, useState, StrictMode } from "react";
+import { type HeadFC, type PageProps, navigate } from "gatsby";
+import { Button, Card, StyledEngineProvider, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import config from "../../config";
+import { ThemeToggle } from "squarecomponents";
+import "../stylesheets/index.css";
+import "../stylesheets/common.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const isBrowser = typeof window !== "undefined";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+export const Head: HeadFC = () => <title>{config.appName}</title>;
 
-const NotFoundPage: React.FC<PageProps> = () => {
+const NotFoundPage: FC<PageProps> = (props) => {
+  // get stuff from local storage
+  let localStorageTheme;
+  if (isBrowser) {
+    localStorageTheme = window.localStorage.getItem("theme");
+  } else {
+    localStorageTheme = null;
+  }
+  let defaultThemeState: "dark" | "light";
+  if (localStorageTheme !== null) {
+    defaultThemeState = localStorageTheme === "dark" ? "dark" : "light";
+  } else {
+    defaultThemeState = config.defaultThemeState;
+    if (isBrowser) {
+      window.localStorage.setItem("theme", config.defaultThemeState);
+    }
+  }
+  // state
+  const [themeState, changeThemeState] = useState(defaultThemeState);
+
+  // functions
+
+  const customChangeThemeState = (newThemeState: "dark" | "light") => {
+    changeThemeState(newThemeState);
+    if (isBrowser) {
+      window.localStorage.setItem("theme", newThemeState);
+    }
+  };
+
+  const navtigateToHome = async () => {
+    await navigate("/");
+  };
+
+  // misc
+  let currentTheme = createTheme({
+    palette: {
+      mode: themeState,
+    },
+    typography: {
+      fontFamily: config.defaultFont,
+    },
+  });
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <StrictMode>
+      <ThemeProvider theme={currentTheme}>
+        <StyledEngineProvider injectFirst>
+          <Card className="main" square>
+            <div className="inside-main">
+              <Typography align="center" color="error" variant="h1">
+                404
+              </Typography>{" "}
+              <Typography align="center" color="error" variant="subtitle1">
+                {props.location.pathname.length > 10
+                  ? props.location.pathname.slice(0, 10) + "..."
+                  : props.location.pathname}{" "}
+                page does not exist.
+              </Typography>
+              <Button
+                variant="contained"
+                size="large"
+                onClick={navtigateToHome}
+              >
+                go to home page
+              </Button>
+              <ThemeToggle
+                themeState={themeState}
+                customChangeThemeState={customChangeThemeState}
+              />
+            </div>
+          </Card>
+        </StyledEngineProvider>
+      </ThemeProvider>
+    </StrictMode>
+  );
+};
 
-export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
+export default NotFoundPage;
